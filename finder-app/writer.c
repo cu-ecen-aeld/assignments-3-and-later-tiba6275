879f148338f45b108cb6a8fefba47bf57b600cc2
Tim Bailey <tiba6275@colorado.edu>
2023-09-09 22:50:01 -0400

    Add comments

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,12 +1,24 @@
-// Writer code for assignment 2
-// Author: Tim Bailey
-
+/* Writer code for CU Boulder ECEN 5713, assignment 2.
+ * Author: Tim Bailey, tiba6275@colorado.edu
+ * Date: 9/9/2023
+ *
+ * For educational use only.
+ */
+ 
 #include <stdio.h>
 #include <stdlib.h>
 #include <syslog.h>
 
 #define NUM_PARAMS 2
 
+/* Writes an input string to a file. Assumes directory is created by caller.
+ * Exits and logs on file creation error.
+ * Params: 
+ *   writefile - string location of file
+ *   writestr - string to write to file
+ * Returns:
+ *   exits with 1 on file creation error
+ */
 void write(char *writefile, char *writestr) {
     FILE *fp;
     fp = fopen(writefile, "w");
@@ -21,8 +33,9 @@ void write(char *writefile, char *writestr) {
     syslog(LOG_DEBUG, "%s written to %s.", writestr, writefile);
 }
 
+/* Writes arg2 to location arg1. Syslog debug and errors to LOG_USER. */
 int main(int argc, char *argv[]) {
-    openlog("Logger", LOG_CONS | LOG_PID, LOG_USER);
+    openlog("AESD A2", LOG_CONS | LOG_PID, LOG_USER);
     
     if (argc != (NUM_PARAMS + 1)) {
     	syslog(LOG_ERR, "Incorrect number of parameters, should be %d.", NUM_PARAMS);
